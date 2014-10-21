1d750bfefd4fd649c3d2d151988e812cf4ba15e0
Tomas Jacik <foxycode@sunfox.cz>
2014-10-21 22:03:26 +0000

    zfsnap pro lepsi praci se snapshoty

--- backup_zfs.sh
+++ backup_zfs.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-ZONES=`zfs list -H -o name | grep "^zones\/[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}" | sed 's/zones\///g'`
-# SNAPS=`zfs list -H -t snapshot -o name`
-IMAGES=`imgadm list -H -o uuid`
+ZFSNAP=/backup/scripts/zfsnap/sbin/zfsnap.sh
+ZONES=$(zfs list -H -o name | grep "^zones\/[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}" | sed 's/zones\///g')
+IMAGES=$(imgadm list -H -o uuid)
+HOUR=$(date +%H)
 
-today=`date +%Y-%m-%d`
-old=`TZ=GMT+119 date +%Y-%m-%d`
-
-for z in $ZONES; do
-    if [[ ! $IMAGES =~ $z ]]; then
-        for s in `zfs list -H -t snapshot -o name | grep $z@ | sed 's/.*@//g'`; do
-            if [[ $s == $old ]]; then
-                zfs destroy zones/$z@$s
-            fi
-            if [[ $s == $today ]]; then
-                zfs destroy zones/$z@$s
-            fi
-        done
-        zfs snapshot zones/$z@$today
+for zone in $ZONES; do
+    if [[ ! $IMAGES =~ $zone ]]; then
+        if [ "$HOUR" == "01" ]; then
+            $ZFSNAP snapshot -a 1w -z zones/$zone
+        else
+            $ZFSNAP snapshot -a 1d -z zones/$zone
+        fi
+        $ZFSNAP destroy zones/$zone
         #zfs send zones/$z@$today | gzip -9 > /zones/backup/$z.$today.gz
     fi
 done
---
+++ zfsnap/sbin/zfsnap.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# This file is licensed under the BSD-3-Clause license.
+# See the AUTHORS and LICENSE files for more information.
+#
+# wiki:             https://github.com/zfsnap/zfsnap/wiki
+# repository:       https://github.com/zfsnap/zfsnap
+# bug tracking:     https://github.com/zfsnap/zfsnap/issues
+
+# import zfsnap's library
+ABSOLUTE_ZFSNAP=`readlink -f "$0"`
+ZFSNAP_LIB_DIR="${ZFSNAP_LIB_DIR:-${ABSOLUTE_ZFSNAP%/*/zfsnap.sh}/share/zfsnap}"
+. "$ZFSNAP_LIB_DIR/core.sh"
+
+## FUNCTIONS
+
+Help() {
+    cat << EOF
+${0##*/} v${VERSION}
+
+Syntax:
+${0##*/} [ options ] | <command> [ options ] zpool/filesystem ...
+
+COMMANDS:
+`for C in $ZFSNAP_LIB_DIR/commands/*.sh; do [ -f "$C" ] && C_NAME="${C##*/}" && printf '  %s\n' "${C_NAME%.*}"; done`
+
+OPTIONS:
+  -h, --help        = Print this help and exit
+  -V, --version     = Print the version number and exit
+
+MORE HELP:
+  All commands accept the -h option; use it for command-specific help.
+  Example: ${0##*/} snapshot -h
+
+LINKS:
+  wiki:             https://github.com/zfsnap/zfsnap/wiki
+  repository:       https://github.com/zfsnap/zfsnap
+  bug tracking:     https://github.com/zfsnap/zfsnap/issues
+
+EOF
+    Exit 0
+}
+
+case "$1" in
+    '-h'|'--help'|'')
+        Help;;
+    '-V'|'--version')
+        printf '%s v%s\n' "${0##*/}" "${VERSION}"; Exit 0;;
+    *)
+        CMD="$1"
+        if [ -f "$ZFSNAP_LIB_DIR/commands/${CMD}.sh" ]; then
+            shift
+            . "$ZFSNAP_LIB_DIR/commands/${CMD}.sh"
+        else
+            Fatal "'$CMD' is not a valid ${0##*/} command."
+        fi
+        ;;
+esac
+
+Exit 0
+# vim: set ts=4 sw=4 expandtab:
