4de295089f0300211725cd876cde47559ad150da
Tomas Jacik <tomas@jacik.cz>
2019-10-12 14:57:33 +0200

    zfs_create_backup_snapshots.pl: added delete part, get rid of zfsnap

--- zfs_create_backup_snapshots.pl
+++ zfs_create_backup_snapshots.pl
@@ -6,6 +6,7 @@ use warnings;
 use Getopt::Long;
 use Pod::Usage;
 use Time::Piece;
+use Time::Seconds;
 
 use FindBin qw($Bin);
 use lib "$Bin/libs";
@@ -17,6 +18,8 @@ use Terminal qw(colorize);
 
 my $ZFS = '/usr/sbin/zfs';
 my $ZFSNAP = '/backup/scripts/zfsnap/sbin/zfsnap.sh';
+my $DATE_PATTERN = '^20[0-9][0-9]-[01][0-9]-[0-3][0-9]_[0-2][0-9]\.[0-5][0-9]\.[0-5][0-9]$';
+my $TTL_PATTERN = '^([0-9]{1})([wd]+)$';
 my $TIME_FORMAT = '%Y-%m-%d_%H.%M.%S';
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -56,6 +59,35 @@ if (defined $vmid) {
 chomp @zones;
 
 # ----------------------------------------------------------------------------------------------------------------------
+# Local functions
+# ----------------------------------------------------------------------------------------------------------------------
+
+sub findExpiredSnapshots {
+    my ($fs) = @_;
+    my @expired = ();
+
+    chomp(my @snapshots = `$ZFS list -Ho name -t snapshot -r $fs`);
+    for my $snapshot (@snapshots) {
+        my $snapshotName = substr($snapshot, rindex($snapshot, '@') + 1);
+        my ($snapDate, $ttl) = split /--/, $snapshotName;
+        if ($snapDate =~ /$DATE_PATTERN/ && $ttl =~ /$TTL_PATTERN/) {
+            my $dt = Time::Piece->strptime($snapDate, $TIME_FORMAT);
+            my ($ttlCount, $ttlUnit) = ($ttl =~ /$TTL_PATTERN/);
+            if ($ttlUnit eq 'w') {
+                $dt += ONE_DAY * $ttlCount * 7;
+            } else {
+                $dt += ONE_DAY * $ttlCount;
+            }
+            if ($dt < localtime) {
+                push @expired, $snapshot;
+            }
+        }
+    }
+
+    return @expired;
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
 # Process zones
 # ----------------------------------------------------------------------------------------------------------------------
 
@@ -87,24 +119,34 @@ for my $uuid (@zones) {
         system("zfs snapshot $disk\@$snapName");
     }
 
-    chomp(my $migrateRunning = `ps ax | grep [m]igrate_vm\.pl`);
+    chomp(my $migrateRunning = `ps ax | grep "[m]igrate_vm\.pl"`);
     if ($migrateRunning eq '') {
         if ($verbose) {
-            print colorize("<light_green>Pruning old snapshots for VM</light_green> $alias\n");
-            print colorize(" <blue>*</blue> $zfs_filesystem\n");
+            print colorize("<light_green>Deleting old snapshots for VM</light_green> $alias\n");
         }
-        system("/backup/scripts/zfsnap/sbin/zfsnap.sh destroy $zfs_filesystem") and do {
-            print " \e[31m* Error\e[m: can't prune, aborting\n";
-            exit 1;
-        };
-        for my $disk (@disks) {
+
+        my @expired = findExpiredSnapshots($zfs_filesystem);
+        for my $snapshot (@expired) {
             if ($verbose) {
-                print colorize(" <blue>*</blue> $disk\n");
+                print colorize(" <blue>*</blue> $snapshot\n");
             }
-            system("/backup/scripts/zfsnap/sbin/zfsnap.sh destroy $disk") and do {
-                print " \e[31m* Error\e[m: can't prune, aborting\n";
+            system("zfs destroy $snapshot") and do {
+                print " \e[31m* Error\e[m: can't destroy snapshot, aborting\n";
                 exit 1;
             };
+        }
+
+        for my $disk (@disks) {
+            my @expired = findExpiredSnapshots($disk);
+            for my $snapshot (@expired) {
+                if ($verbose) {
+                    print colorize(" <blue>*</blue> $snapshot\n");
+                }
+                system("zfs destroy $snapshot") and do {
+                    print " \e[31m* Error\e[m: can't destroy snapshot, aborting\n";
+                    exit 1;
+                };
+            }
         }
     }
 }
